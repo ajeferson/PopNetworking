51c4f1c07d909eea18265a7c435f6921d9a98d7b
Alan Jeferson <alan.jeferson11@gmail.com>
2018-04-23 08:04:57 -0300

    Initial pod setup

--- Example/PopNetworking/ViewController.swift
+++ Example/PopNetworking/ViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
+import PopNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(String.hello)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
---
+++ PopNetworking/Classes/StringExtension.swift
@@ -0,0 +1,16 @@
+//
+//  StringExtension.swift
+//  Nimble
+//
+//  Created by Alan Jeferson on 23/04/2018.
+//
+
+import Foundation
+
+public extension String {
+    
+    static var hello: String {
+        return "Hello, World!"
+    }
+    
+}
